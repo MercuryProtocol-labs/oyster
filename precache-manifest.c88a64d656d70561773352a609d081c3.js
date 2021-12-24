69a929dc050830061451047e1e675c35a9767314
bolingboling <zhangxiaoshang66@163.com>
2021-12-24 16:17:32 +0800

    Updates

---
+++ precache-manifest.c88a64d656d70561773352a609d081c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751f32db133b42ed846784f2c60a5c1e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "661a85be23ec98912efc",
+    "url": "./static/css/2.fbcbe827.chunk.css"
+  },
+  {
+    "revision": "3f77d40e01268425a008",
+    "url": "./static/css/main.0ffa2c02.chunk.css"
+  },
+  {
+    "revision": "661a85be23ec98912efc",
+    "url": "./static/js/2.59b12af6.chunk.js"
+  },
+  {
+    "revision": "4db639e2592e4768ce4a9f50532ef4b7",
+    "url": "./static/js/2.59b12af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f77d40e01268425a008",
+    "url": "./static/js/main.9c9e999d.chunk.js"
+  },
+  {
+    "revision": "ec5687634d9507c6e22c",
+    "url": "./static/js/runtime-main.0a431f8f.js"
+  }
+]);
